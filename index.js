3a42e4a832e5aa175ce7c72c941c0bbb887a6701
PavelNovik <92117738+PavelNovik@users.noreply.github.com>
2023-03-19 20:16:07 +0300

    1st ex compl

---
+++ index.js
@@ -0,0 +1,33 @@
+// class Class {
+//   static getNumber(i) {
+//     i = 0.5;
+//     i *= 2;
+
+//     return 1 * i;
+//   }
+// }
+// let varia = Class.getNumber();
+// console.log(varia);
+// varia = Class.getNumber();
+// console.log(varia);
+// varia = Class.getNumber();
+// console.log(varia);
+// varia = Class.getNumber();
+// console.log(varia);
+// varia = Class.getNumber();
+// console.log(varia);
+// varia = Class.getNumber();
+// console.log(varia);
+/////////////////////////////////////////
+// function sumDigits(number) {
+//   return Math.abs(number)
+//     .toString()
+//     .split('')
+//     .reduce((sum, n) => {
+//       return sum + +n;
+//     }, 0);
+// }
+
+// console.log(sumDigits(99));
+// console.log(sumDigits(-994));
+// console.log(sumDigits(9));
